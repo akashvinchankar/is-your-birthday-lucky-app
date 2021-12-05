9ae5079174711327c8776a265fb295dda6762baf
akashvinchankar <86399324+akashvinchankar@users.noreply.github.com>
2021-12-05 17:58:38 +0530

    styling and javascript

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
 
+function comapareValues(sum, luckyNumber){
+    if(sum % luckyNumber === 0){
+        outputBox.innerText = "Your birthday is lucky!";
+    }else{
+        outputBox.innerText = "Your birthday is not lucky!";
+    }
+}
 
+function checkNumberIsLucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    comapareValues(sum, luckyNumber.value)
+}
+
+function calculateSum(dob){
+    dob = dob.replaceAll("-", "");
+    let sum = 0;
+    for(let i=0; i<dob.length; i++){
+        sum += Number(dob.charAt(i));
+    }
+    return sum;
+}
+
+checkNumberButton.addEventListener("click", function getValues(){
+    checkNumberIsLucky();
+});
